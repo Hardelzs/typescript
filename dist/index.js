d0bbd5d835b23454d8030466614d8f9d6785340b
Hardelz <135560555+Hardelzs@users.noreply.github.com>
2025-06-06 21:06:57 +0100

    Refactor calculateTax function to set default tax year and add employee object with retire method

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,17 @@
 "use strict";
-function calculateTax(income, tasyear) {
+function calculateTax(income, tasyear = 2022) {
     if (tasyear < 2022)
         return income * 1.2;
     return income * 1.5;
 }
 const total = calculateTax(1000, 2022);
 console.log(total);
+let employee = {
+    id: 1,
+    name: "John Doe",
+    retire: (date) => {
+        console.log(date);
+    }
+};
+employee.retire(new Date());
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@
 
 
 
-function calculateTax(income: number, tasyear: 2022) : number {
+function calculateTax(income: number, tasyear: number = 2022) : number {
   if (tasyear < 2022)
     return income * 1.2
   return income * 1.5 // 15000
@@ -16,8 +16,19 @@ function calculateTax(income: number, tasyear: 2022) : number {
 const total = calculateTax(10_00, 2022)
 console.log(total);
 
+let employee : {
+  readonly id: number,
+  name: string
+  retire: (date: Date) => void
 
-
+} = {
+  id: 1,
+  name:  "John Doe",
+  retire: (date: Date) => {
+    console.log(date);
+  }
+}
+employee.retire(new Date())
 
 
 
